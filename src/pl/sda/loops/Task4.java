a26aab0eddb4025a4aba13507c17083cdfe567cb
bartmilew358 <bartmilew@gmail.com>
2019-03-02 15:59:21 +0100

    add taks/item

--- src/pl/sda/loops/Task4.java
+++ src/pl/sda/loops/Task4.java
@@ -1,4 +1,19 @@
 package pl.sda.loops;
 
 public class Task4 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean theSameStrings (String text1, String text2) {
+
+
+
+
+
+    }
+
+
+
 }
