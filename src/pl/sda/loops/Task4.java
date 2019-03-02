410bb56a95b259631942b8cd69b05cc03db51acf
bartmilew358 <Kazikrobigalepokiblach358>
2019-03-02 19:39:01 +0100

    lopps tasks update

--- src/pl/sda/loops/Task4.java
+++ src/pl/sda/loops/Task4.java
@@ -4,16 +4,27 @@ public class Task4 {
 
     public static void main(String[] args) {
 
-    }
-
-    public static boolean theSameStrings (String text1, String text2) {
-
-
-
-
+        theSameStrings("Janusz", "Janusz");
 
     }
 
+    public static void theSameStrings(String text1, String text2) {
+
+        if (text1.length() != text2.length()) System.out.println("Teksty są różne");
+
+        for (int i = 0; i < text1.length(); i++) {
+
+         if (text1.charAt(i) == text2.charAt(i)) {
+
+         }   else
+             {
+                 System.out.println("Teksty są różne.");
+                 break;
+         }
+            System.out.println("Teksty są takie same.");
+        }
+
+    }
 
 
 }
