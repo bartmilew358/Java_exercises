efe3fff7ec6916c79263439d41ff182661418372
bartmilew358 <Kazikrobigalepokiblach358>
2019-03-04 21:03:08 +0100

    update task 5 #loops

--- src/pl/sda/loops/Task5.java
+++ src/pl/sda/loops/Task5.java
@@ -9,20 +9,22 @@ public class Task5 {
 
     public static void main(String[] args) {
 
-        countPhrase("pizda", "pizza");
+        System.out.println("Ilość powtórzeń phrase w text = ");
+        System.out.println(countPhrase("pi", "pieczarkatypupipi"));
     }
 
+    public static int countPhrase (String phrase, String text) {
 
+        int i=0;
+        int var = 0;
 
+        while (text.indexOf(phrase, i) >= 0) {
 
+       var++;
+       i = text.indexOf(phrase, i) + 1;
 
-
-
-
-    public static void countPhrase (String phrase, String text) {
-
-
-
+        }
+       return var;
     }
 
 
