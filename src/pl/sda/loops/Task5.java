8cb1846ccef0512a1acd6eec2b67a4ee866fbcca
bartmilew358 <bartmilew@gmail.com>
2019-03-04 13:45:13 +0100

    String task5

---
+++ src/pl/sda/loops/Task5.java
@@ -0,0 +1,30 @@
+package pl.sda.loops;
+
+/*
+* Napisz metodę sprawdzającą ilość wystąpień frazy: phrase w tekście: text. Parametry metody to: phrase i text.
+Podpowiedź: użyj metody klasy String która sprawdza index dla podanej frazy
+* */
+
+public class Task5 {
+
+    public static void main(String[] args) {
+
+        countPhrase("pizda", "pizza");
+
+    }
+
+
+
+
+
+
+
+
+    public static void countPhrase (String phrase, String text) {
+
+
+
+    }
+
+
+}
