c210f7c7925fe4bdd2f249aad07ccc4cde62e6b6
bartmilew358 <Kazikrobigalepokiblach358>
2019-03-08 22:41:57 +0100

    update tasks #arrays

---
+++ src/pl/sda/encapsulation/task/Item/ItemManager.java
@@ -0,0 +1,7 @@
+package pl.sda.encapsulation.task.Item;
+
+public class ItemManager {
+    ItemModel createShopItem(String title, String description, double price) {
+        return new ItemModel();
+    }
+}
