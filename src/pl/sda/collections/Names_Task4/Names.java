f9833588e8c95399fae3e735051f86e08a985d7e
bartmilew358 <bartmilew@gmail.com>
2019-03-10 16:00:29 +0100

    collections tsks

---
+++ src/pl/sda/collections/Names_Task4/Names.java
@@ -0,0 +1,34 @@
+//package pl.sda.collections.Names_Task4;
+//
+//import java.util.Scanner;
+//
+//public class Names {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner scanner = new Scanner(System.in);
+//        while (true) {
+//            System.out.print("Podaj imie >>> ");
+//            String input = scanner.nextLine();
+//
+//            if ("q".equalsIgnoreCase(input)) {
+//                break;
+//            }
+//
+//            int num = getNumber(input);
+//                double sqrt = getSqrt(num);
+//                System.out.println("Sqrt for " + num + " = " + sqrt);
+//        }
+//    }
+//
+//    private static int getNumber(String input) {
+//        String imie;
+//        imie = String.valueOf(input);
+//        return ;
+//    }
+//
+//    private static double getSqrt(int num) {
+//        return Math.sqrt(num);
+//    }
+//
+//}
