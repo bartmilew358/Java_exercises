e916310170a6c3650f9720245d206ebb503af026
bartmilew358 <bartmilew@gmail.com>
2019-03-24 16:54:57 +0100

    enum deck / pkr

--- src/pl/sda/fx/Calc.java
+++ src/pl/sda/fx/Calc.java
@@ -1,5 +1,7 @@
 package pl.sda.fx;
 
+import javafx.event.ActionEvent;
+
 import java.awt.*;
 
 public class Calc {
@@ -7,11 +9,12 @@ public class Calc {
     public static void main(String[] args) {
 
 
-
     }
 
+    public void show1(ActionEvent actionEvent) {
 
 
 
 
+    }
 }
