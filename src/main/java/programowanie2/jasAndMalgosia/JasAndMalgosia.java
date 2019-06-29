f835971bcf5ae4bc9212160e7cd4730df6b1885a
bartmilew358 <bartmilew@gmail.com>
2019-06-29 14:05:13 +0200

    Thread - jasAndMalgosia countDown

--- src/main/java/programowanie2/jasAndMalgosia/JasAndMalgosia.java
+++ src/main/java/programowanie2/jasAndMalgosia/JasAndMalgosia.java
@@ -1,69 +1,71 @@
 package programowanie2.jasAndMalgosia;
 
+import java.util.concurrent.CountDownLatch;
+
 public class JasAndMalgosia {
+
+    public static CountDownLatch countDownLatch = new CountDownLatch(2);
 
     public static void main(String[] args) throws InterruptedException {
 
         Thread jas;
-        jas = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    System.out.println("Jaś robi śniadanie.");
-                    Thread.sleep(5000);
-                    System.out.println("Jaś zjadł i klasycznie zostawił talerz na stole.");
-                    System.out.println("Jaś walczy z prysznicem.");
-                    Thread.sleep(3000);;
-                    System.out.println("Jaś skończył się myć.");
-                    System.out.println("Jaś się ubiera na paradę równości.");
-                    Thread.sleep(1000);
-                    System.out.println("Jaś się ubrał ale jednak pomylił daty. Parady dziś nie ma wiec wychodzi tak ubrany do pracy.");
-                    System.out.println("Jaś idzie po zakupy do sklepu ze zdrową żywnością.");
-                    Thread.sleep(15000);
-                    System.out.println("Jaś wrócił z zakupów.");
-                    System.out.println("Jaś gra na konsoli w symulator tindera.");
-                    Thread.sleep(5000);
-                    System.out.println("Jaś wygrałm i skończył.");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        jas = new Thread(() -> {
+            try {
+                System.out.println("Jaś robi śniadanie.");
+                Thread.sleep(5000);
+                System.out.println("Jaś zjadł i klasycznie zostawił talerz na stole.");
+                System.out.println("Jaś walczy z prysznicem.");
+                Thread.sleep(3000);
+                System.out.println("Jaś skończył się myć.");
+                System.out.println("Jaś się ubiera na paradę równości.");
+                Thread.sleep(1000);
+                System.out.println("Jaś się ubrał ale jednak pomylił daty. Parady dziś nie ma wiec wychodzi tak ubrany do pracy.");
+                System.out.println("Jaś idzie po zakupy do sklepu ze zdrową żywnością.");
+                Thread.sleep(15000);
+                System.out.println("Jaś wrócił z zakupów.");
+                System.out.println("Jaś gra na konsoli w symulator tindera.");
+                Thread.sleep(5000);
+                System.out.println("Jaś wygrał i skończył.");
+                JasAndMalgosia.countDownLatch.countDown();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
         );
 
         Thread malgosia;
-        malgosia = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    System.out.println("Małgosia idzie biegać rano jak każdy.");
-                    Thread.sleep(6000);
-                    System.out.println("Małgosia skończyła biegać i udostępniła wynik na endomondo.");
-                    System.out.println("Małgosia się podmywa");
-                    Thread.sleep(2000);
-                    System.out.println("Małgosia się ogarneła.");
-                    System.out.println("Małgosia robi iphhonem zdjęcie śniadania.");
-                    Thread.sleep(1000);
-                    System.out.println("Małgosia już wybrała filtry i skończyła śniadanie.");
-                    System.out.println("Małgosia ora wsyzstkich domowników problemem, że nie ma co ubrać.");
-                    Thread.sleep(1000);
-                    System.out.println("Małgosia się ubrała.");
-                    System.out.println("Gosia spotyka się z koleżanką (dziwe).");
-                    Thread.sleep(25000);
-                    System.out.println("Gosia wróciła i robi afere na chacie o nieumyty talerz.");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        malgosia = new Thread(() -> {
+            try {
+                System.out.println("Małgosia idzie biegać rano jak każdy.");
+                Thread.sleep(6000);
+                System.out.println("Małgosia skończyła biegać i udostępniła wynik na endomondo.");
+                System.out.println("Małgosia się podmywa");
+                Thread.sleep(2000);
+                System.out.println("Małgosia się ogarneła.");
+                System.out.println("Małgosia robi iphhonem zdjęcie śniadania.");
+                Thread.sleep(1000);
+                System.out.println("Małgosia już wybrała filtry i skończyła śniadanie.");
+                System.out.println("Małgosia ora wsyzstkich domowników problemem, że nie ma co ubrać.");
+                Thread.sleep(1000);
+                System.out.println("Małgosia się ubrała.");
+                System.out.println("Gosia spotyka się z koleżanką (dziwe).");
+                Thread.sleep(25000);
+                System.out.println("Gosia wróciła i robi afere na chacie o nieumyty talerz.");
+                JasAndMalgosia.countDownLatch.countDown();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
         );
 
-    jas.start();
-    malgosia.start();
+        jas.start();
+        malgosia.start();
 
-    jas.join();
-    malgosia.join();
+//        jas.join();
+//        malgosia.join();
 
+        // jak się skońńczy coundawn u Jasia to odejmie 1 i potem jak się skończy countdawn u Małgosi to też odejmie 1 i ak bedzie zero to await i wyjazd.
+        countDownLatch.await();
         System.out.println("KONIEC DNIA. ELO SZLAUFY.");
     }
 }
